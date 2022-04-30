fff8d49e3f177e141e738fa5a7aab61ad1f01700
Lucas Baccillere <lbaccillere@outlook.com>
2022-04-30 10:00:16 -0300

    feat: add crud user and auth

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: false,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Rest Api Nearby')
+    .setDescription('Search Restaurants Nearby')
+    .setVersion('1.0')
+    .addTag('Rest API')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
